5cda639e0c9c4dcefa36a4f154676dcb146ba3c7
DevLemos <kauancode@outlook.com>
2026-05-21 12:40:44 -0300

    :feat: sparkles: Inicio do Projeto

---
+++ KodeFlow/Controllers/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace KodeFlow.Controllers;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+}

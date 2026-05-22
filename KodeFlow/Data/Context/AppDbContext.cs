2e003c89150205858e10cc6c12d00e6c722a6576
DevLemos <kauancode@outlook.com>
2026-05-22 13:27:49 -0300

    :sparkles: feat: cria DbContext e registra injeção de dependência

---
+++ KodeFlow/Data/Context/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using KodeFlow.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace KodeFlow.Data.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+    public DbSet<Tutor> Tutores { get; set; }
+    public DbSet<Contato> Contatos { get; set; }
+    public DbSet<Endereco> Enderecos { get; set; }
+    public DbSet<Animal> Animais { get; set; }
+    public DbSet<Raca> Racas { get; set; }
+    public DbSet<Especie> Especies { get; set; }
+    public DbSet<Consulta> Consultas { get; set; }
+    public DbSet<Prontuario> Prontuarios { get; set; }
+    public DbSet<Veterinario> Veterinarios { get; set; }
+    public DbSet<Especialidade> Especialidades { get; set; }
+}

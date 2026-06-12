03d5e710412f248136590409a4742b4d72fd3c67
DevLemos <kauancode@outlook.com>
2026-06-12 13:20:34 -0300

    :sparkles: feat: Usando FluentAPI para amarrar melhor o relacionamento entre tutor, contado e endereço.

--- KodeFlow/Data/Context/AppDbContext.cs
+++ KodeFlow/Data/Context/AppDbContext.cs
@@ -19,4 +19,20 @@ public class AppDbContext : DbContext
     public DbSet<Prontuario> Prontuarios { get; set; }
     public DbSet<Veterinario> Veterinarios { get; set; }
     public DbSet<Especialidade> Especialidades { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Tutor>()
+            .HasOne(t => t.Contato)
+            .WithOne(c => c.Tutor)
+            .HasForeignKey<Contato>(c => c.TutorId);
+
+        modelBuilder.Entity<Tutor>()
+            .HasOne(t => t.Endereco)
+            .WithOne(e => e.Tutor)
+            .HasForeignKey<Endereco>(e => e.TutorId);
+
+    }
 }

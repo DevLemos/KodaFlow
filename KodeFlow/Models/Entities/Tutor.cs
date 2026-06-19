b00ac89691e58f35ca1bc734bf75cd2908c87b06
DevLemos <kauancode@outlook.com>
2026-06-19 13:48:17 -0300

    :sparkles: feat: Criando validação personalizada (Attribute)

--- KodeFlow/Models/Entities/Tutor.cs
+++ KodeFlow/Models/Entities/Tutor.cs
@@ -1,7 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using KodeFlow.Validations;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
-using KodeFlow.Validations;
 
 namespace KodeFlow.Models.Entities;
 
